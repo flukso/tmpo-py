ffea6a7286ae7a93e73667041a0750fe6102fc1a
Jimmy Dinis <jimmydinis@smartrisks.fr>
2015-01-29 18:21:49 +0100

    Os checking
    Check os type, for creating local tmpo folder (.tmpo not allowed in windows)

--- tmpo.py
+++ tmpo.py
@@ -124,7 +124,10 @@ class Session():
         self.debug = False
         if path is None:
             path = os.environ["HOME"]
-        self.home = path + os.sep + ".tmpo"
+        if _platform == "linux" or _platform == "linux2":
+            self.home = path + os.sep + ".tmpo"
+        elif _platform == "win32":
+            self.home = path + os.sep + "tmpo"
         self.db = self.home + os.sep + "tmpo.sqlite3"
         self.crt = self.home + os.sep + "flukso.crt"
         self.host = "api.flukso.net"
