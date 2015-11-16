503539165d9c4ec3f579ca7721ece1a9d4be4890
Roel De Coninck <roeldeconinck@gmail.com>
2015-11-16 17:30:49 +0100

    Adding MANIFEST.in and restructuring of the code to successfully add files and build

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Get the version from the source code
-with open(path.join(here, 'tmpo.py'), encoding='utf-8') as f:
+with open(path.join(here, 'tmpo', '__init__.py'), encoding='utf-8') as f:
     lines = f.readlines()
     for l in lines:
         if l.startswith('__version__'):
@@ -82,7 +82,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    #py_modules=["tmpo.py"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -103,14 +103,14 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'tmpo': ['tmpo.ipynb', 'LICENSE', 'readme.md'],
+        'tmpo': ['tmpo.ipynb', 'LICENSE', 'README.md'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    #data_files=['README.md'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
