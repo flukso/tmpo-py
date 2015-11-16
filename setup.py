c6bcdfd64f4bb23dba5af64c59a0768c3279c3bf
Roel De Coninck <roeldeconinck@gmail.com>
2015-11-16 22:04:38 +0100

    Removing unused fields and unnecessary comments in setup.py

--- setup.py
+++ setup.py
@@ -29,22 +29,12 @@ with open(path.join(here, 'tmpo', '__init__.py'), encoding='utf-8') as f:
 
 setup(
     name='tmpo',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=__version__,
-
-    description='A python client to tmpo',
+    description='A python client for the tmpo protocol for timeseries',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/flukso/tmpo-py',
-
-    # Author details
     author='Bart Van Der Meerssche',
-    
-    # Choose your license
+    author_email='bart@flukso.net',
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -73,7 +63,6 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
 
-    # What does your project relate to?
     keywords='data monitoring tmpo timeseries sqlite',
 
     # You can just specify the packages manually here if your project is
@@ -85,39 +74,15 @@ setup(
     #py_modules=["tmpo.py"],
 
     # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    # your project is installed. 
     install_requires=['numpy', 'pandas', 'futures', 'requests_futures'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
+    # Note: for creating the source distribution, they had to be included in the
+    # MANIFEST.in as well. 
     package_data={
         'tmpo': ['tmpo.ipynb', 'LICENSE', 'README.md'],
     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=['README.md'],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
 )
