1054f47f2e57047a58bad6c06e0705a93c5c7ee3
Jan Pecinovsky <janpecinovsky@gmail.com>
2017-06-20 11:39:20 +0200

    moved head and tail default values to deal with None

--- tmpo/__init__.py
+++ tmpo/__init__.py
@@ -213,10 +213,18 @@ class Session():
         return slist
 
     @dbcon
-    def series(self, sid, recycle_id=None, head=0, tail=EPOCHS_MAX,
+    def series(self, sid, recycle_id=None, head=None, tail=None,
                datetime=True):
-        head = self._2epochs(head)
-        tail = self._2epochs(tail)
+        if head is None:
+            head = 0
+        else:
+            head = self._2epochs(head)
+
+        if tail is None:
+            tail = EPOCHS_MAX
+        else:
+            tail = self._2epochs(tail)
+
         if recycle_id is None:
             self.dbcur.execute(SQL_TMPO_RID_MAX, (sid,))
             recycle_id = self.dbcur.fetchone()[0]
@@ -237,9 +245,17 @@ class Session():
             return pd.Series([], name=sid)
 
     @dbcon
-    def dataframe(self, sids, head=0, tail=EPOCHS_MAX, datetime=True):
-        head = self._2epochs(head)
-        tail = self._2epochs(tail)
+    def dataframe(self, sids, head=None, tail=None, datetime=True):
+        if head is None:
+            head = 0
+        else:
+            head = self._2epochs(head)
+
+        if tail is None:
+            tail = EPOCHS_MAX
+        else:
+            tail = self._2epochs(tail)
+            
         series = [self.series(sid, head=head, tail=tail, datetime=False)
                   for sid in sids]
         df = pd.concat(series, axis=1)
