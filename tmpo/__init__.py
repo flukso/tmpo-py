255f686291f2be86cdbba491bf3ecc317aae73db
Jan Pecinovsky <janpecinovsky@gmail.com>
2018-06-14 18:02:34 +0200

    split apisession - sqlitesession

--- tmpo/__init__.py
+++ tmpo/__init__.py
@@ -1,8 +1,9 @@
 __title__ = "tmpo"
-__version__ = "0.2.10"
+__version__ = "0.3.0"
 __build__ = 0x000100
 __author__ = "Bart Van Der Meerssche"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017 Bart Van Der Meerssche"
 
 from .sqlitesession import SQLiteSession as Session
+from .apisession import APISession
